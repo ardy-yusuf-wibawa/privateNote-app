946698760d9f1a1aabc2b40a5504e1afb2bd1626
ardy yusuf <ardyyusufw0391@gmail.com>
2023-10-12 22:22:02 +0700

    add: vite.config.js

--- src/components/NoteDetail.jsx
+++ src/components/NoteDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../UI/Card'
+import Card from '../modal/Card'
 import NoteDate from './NoteDate'
 import { callButton } from './NoteButton'
 import { openForm, undoArchived, deleteNote, savedArchived } from '../utils/Toast'
